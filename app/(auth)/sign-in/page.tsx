56bec8d57a138cbdc31a78453b60a7e8214be2f3
Dean Howe <hideanhowe@gmail.com>
2025-08-05 11:17:55 +0100

    sign up form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Metadata } from 'next';
+import { APP_NAME } from '@/lib/constants';
+import CredentialsSignInForm from '@/app/(auth)/sign-in/credentials-signin-form';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+};
+
+const SingInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/wipeout-logo.png"
+              alt={`${APP_NAME} logo`}
+              width={770}
+              height={400}
+              priority={true}
+              className="w-44"
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SingInPage;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFoundPage() {
         height={400}
         priority={true}
         className="w-44"
-      ></Image>
+      />
       <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not found</h1>
         <p className="text-destructive">Could not find requested page</p>

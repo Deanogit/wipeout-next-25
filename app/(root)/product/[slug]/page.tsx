d766789701419861dfdf26af797939fb12556e6d
Dean Howe <hideanhowe@gmail.com>
2025-08-07 17:16:50 +0100

    sessionCartId

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
+
 import { Card, CardContent } from '@/components/ui/card';
 
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
 import ProductPrice from '@/components/shared/product/productPrice';
 import ProductImages from '@/components/shared/product/productImages';
+import AddToCart from '@/components/shared/product/addToCart';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -64,7 +65,16 @@ const ProductDetailsPage = async (props: {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <Button className="w-full">Add To Cart</Button>
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: product.price,
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>

163b97bdf24140e806784307853df3491c8ba0c0
Dean Howe <hideanhowe@gmail.com>
2025-10-08 10:57:53 +0100

    search

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
 import Menu from './menu';
+import CategoryDrawer from './category-drawer';
+import Search from './search';
 
-export default function Header() {
+const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/wipeout-logo.png"
               alt={`${APP_NAME} Logo`}
@@ -17,11 +20,18 @@ export default function Header() {
               priority={true}
               className="w-20 md:w-30"
             />
+            {/* <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span> */}
           </Link>
         </div>
-
+        <div className="hidden md:block">
+          <Search />
+        </div>
         <Menu />
       </div>
     </header>
   );
-}
+};
+
+export default Header;
